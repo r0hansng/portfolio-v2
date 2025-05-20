517b287940bf75063d4de626770d305cf15002c7
Rohan Singh <rohan.singh.en@gmail.com>
2025-05-20 10:36:34 +0530

    fix: add helper function to get the current year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import '../../global.css'
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500 mt-[100px]">
-        <small className="mb-2 block text-xs">
-            © 2025 Rohan. All rights reserved.
-        </small>
+      <small className="mb-2 block text-xs">
+        © {currentYear} Rohan. All rights reserved.
+      </small>
     </footer>
   )
 }
